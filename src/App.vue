73b3a25857fec0638b31a31c0922ab1e8edb82e8
Ann Peng <boaimpact@gmail.com>
2021-02-22 14:19:16 +0800

    add jquery popper

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="invoice-generator">
-    <router-view />
+    <button type="button" class="btn btn-info">Info</button>
   </div>
 </template>
 
 <script>
+import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 export default {
   name: "App"
